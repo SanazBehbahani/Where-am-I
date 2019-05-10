b3369b09a428e4a79e1a8866b590715c0570c41a
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-10 13:27:27 -0700

    setup ball_chaser package

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -118,6 +118,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/robond/Desktop/roboND/version-control/project2/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/robond/Desktop/roboND/version-control/project2/catkin_ws/build/my_robot/cmake_install.cmake")
+  include("/home/robond/Desktop/roboND/version-control/project2/catkin_ws/build/ball_chaser/cmake_install.cmake")
 
 endif()
 
