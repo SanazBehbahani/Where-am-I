0d863850b18a561a441a0262fb333ae7d24cd797
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-14 20:00:08 -0700

    add white ball to the world

--- catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/robond/Desktop/roboND/version-control/project2/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/robond/Desktop/roboND/Go_Chase_It/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
