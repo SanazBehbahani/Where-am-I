03acd5d29d19a80b8ea58c22c26d88a854c0fee2
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-16 22:33:10 +0000

    setup simulation from Go_Chase_It project

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/Where-am-I/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/workspace/catkin_ws/Where-am-I/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/workspace/catkin_ws/Where-am-I/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/workspace/catkin_ws/Where-am-I/catkin_ws/build"
+export PYTHONPATH="/home/workspace/catkin_ws/Where-am-I/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/Where-am-I/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/robond/Desktop/roboND/version-control/project2/catkin_ws/src:/home/robond/Desktop/roboND/Go_Chase_It/catkin_ws/src:/home/workspace/catkin_ws/Where-am-I/catkin_ws/src:$ROS_PACKAGE_PATH"
